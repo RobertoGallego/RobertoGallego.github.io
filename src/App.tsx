5199f55022745712286ff8e9212984c1dc2e5eae
RobertoGallego <rvgallego@hotmail.com>
2025-02-09 22:13:14 +0100

    h2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+// FFE6C9 FFC785 FFA09B
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <>ReveCraft</>
+  return (
+    <div
+      style={{
+        position: "relative",
+        height: "100dvh",
+        width: "100dvw",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "flex-end",
+      }}
+    >
+      <h2
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+        }}
+      >
+        I'm a software engineer building efficient, scalable, and user-friendly
+        applications with modern web technologies. Let's create something great.
+      </h2>
+    </div>
+  )
 }
 
 export default App
