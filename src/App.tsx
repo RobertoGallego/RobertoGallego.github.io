6ca5a4cf498de73cead5f7cc96d16215db5f6f12
RobertoGallego <rvgallego@hotmail.com>
2025-02-12 23:09:24 +0100

    fix res

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,104 @@ import ninjaco from "./assets/ninjaco.png"
 import logo from "./assets/logos.png"
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md"
 
+const HomeCardsFullSize = () => {
+  return (
+    <div className="home-cards-fullsize">
+      <div className="home-cards-list-fullsize" style={{ zIndex: 10 }}>
+        <div style={{ zIndex: 100 }}>
+          <Card title="Store v1" imageUrl={ninjaco} width={204} rotate={-5}>
+            <Chip />
+          </Card>
+        </div>
+
+        <div style={{ zIndex: -1 }}>
+          <Card
+            title="My Setup Macbook"
+            imageUrl={back}
+            createdAt="Jun 1993"
+            width={272}
+            rotate={5}
+          >
+            <Chip />
+            <Chip />
+          </Card>
+        </div>
+      </div>
+
+      <div className="home-cards-list-fullsize" style={{ zIndex: 10 }}>
+        <div style={{ zIndex: -1 }}>
+          <Card
+            title="My Setup Macbook"
+            imageUrl={pexel}
+            createdAt="Jun 1993"
+            width={368}
+            rotate={10}
+          >
+            <Chip />
+            <Chip />
+          </Card>
+        </div>
+        <div style={{ zIndex: 100 }}>
+          <Card
+            title="My Setup Macbook"
+            imageUrl={tech}
+            createdAt="Jun 1993"
+            width={300}
+            rotate={-10}
+          >
+            <Chip />
+            <Chip />
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const HomeCardsMobile = () => {
+  return (
+    <div className="home-cards-mobile">
+      <Card title="Store v1" imageUrl={ninjaco} width={204} rotate={-5}>
+        <Chip />
+      </Card>
+
+      <Card
+        title="My Setup Macbook"
+        imageUrl={back}
+        createdAt="Jun 1993"
+        width={272}
+        rotate={5}
+      >
+        <Chip />
+        <Chip />
+      </Card>
+
+      <Card
+        title="My Setup Macbook"
+        imageUrl={pexel}
+        createdAt="Jun 1993"
+        width={368}
+        rotate={10}
+      >
+        <Chip />
+        <Chip />
+      </Card>
+
+      <Card
+        title="My Setup Macbook"
+        imageUrl={tech}
+        createdAt="Jun 1993"
+        width={300}
+        rotate={-10}
+        zIndex={10}
+      >
+        <Chip />
+        <Chip />
+      </Card>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="container">
@@ -48,64 +146,30 @@ function App() {
 
         <div className="home-content">
           <div className="home-text">
-            <FaQuoteRight size={42} className="home-icon" />
+            <FaQuoteRight className="home-icon" />
 
-            <h1 className="home-title">Crafting Engaging Experiences</h1>
+            <h1 className="home-title">Crafting </h1>
+            <h1 className="home-title">Engaging</h1>
+            <h1 className="home-title">Experiences</h1>
 
             <p
               className="home-description"
-              style={{
-                // zIndex: 1,
-                color: "#ff6347",
-              }}
+              style={
+                {
+                  // zIndex: 1,
+                }
+              }
             >
               {/* I'm a software engineer */}
               Let’s build something amazing.
             </p>
           </div>
 
-          <div className="home-cards-list">
-            <Card title="Store v1" imageUrl={ninjaco} width={204} rotate={-5}>
-              <Chip />
-            </Card>
+          <HomeCardsFullSize />
 
-            
-            <Card
-              title="My Setup Macbook"
-              imageUrl={back}
-              createdAt="Jun 1993"
-              width={272}
-              rotate={5}
-            >
-              <Chip />
-              <Chip />
-            </Card>
-            
-            <Card
-              title="My Setup Macbook"
-              imageUrl={pexel}
-              createdAt="Jun 1993"
-              width={368}
-              rotate={10}
-            >
-              <Chip />
-              <Chip />
-            </Card>
+          <HomeCardsMobile />
 
-            <Card
-              title="My Setup Macbook"
-              imageUrl={tech}
-              createdAt="Jun 1993"
-              width={300}
-              rotate={-10}
-              zIndex={10}
-            >
-              <Chip />
-              <Chip />
-            </Card>
-          </div>
-
-          <div style={{ display: "flex" }}>
+          <div className="home-colors">
             {["#ffc785", "#ffe6c9", "#ffa09b", "#ff6347", "#011526"].map(
               (color) => (
                 <div
@@ -123,23 +187,14 @@ function App() {
 
         <div className="footer">
           <div>
-            <p
-              style={{
-                fontFamily: "Roboto, Poppins, Inter, system-ui",
-                fontSize: 14,
-                fontWeight: "500",
-                paddingBlock: 10,
-              }}
-            >
-              2025 © All rights reserved
-            </p>
+            <p className="footer-text">2025 © All rights reserved</p>
           </div>
 
           <MdOutlineKeyboardDoubleArrowDown size={20} />
         </div>
       </div>
 
-      <div style={{ height: "100vh" }}> example</div>
+      {/* <div style={{ height: "100vh" }}> example</div> */}
     </div>
   )
 }
