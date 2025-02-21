22629743ff98546395feab696e1a685aae7cbe03
RobertoGallego <rvgallego@hotmail.com>
2025-02-21 14:32:26 +0100

    asd

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,34 +6,34 @@ import back from "../../assets/bak.jpg"
 import pexel from "../../assets/pexel.jpg"
 import ninjaco from "../../assets/ninjaco.png"
 import { useEffect, useRef, useState } from "react"
-import bb from "../../assets/gif/0.gif"
-import a from "../../assets/gif/1.gif"
-import b from "../../assets/gif/2.gif"
-import c from "../../assets/gif/3.gif"
-import d from "../../assets/gif/4.gif"
-import e from "../../assets/gif/5.gif"
-import f from "../../assets/gif/6.gif"
-import g from "../../assets/gif/7.gif"
-import h from "../../assets/gif/8.gif"
-import i from "../../assets/gif/9.gif"
-import j from "../../assets/gif/10.gif"
-import k from "../../assets/gif/11.gif"
-import l from "../../assets/gif/12.gif"
-import m from "../../assets/gif/13.gif"
-import n from "../../assets/gif/14.gif"
-import o from "../../assets/gif/15.gif"
-import p from "../../assets/gif/16.gif"
-import q from "../../assets/gif/17.gif"
-import r from "../../assets/gif/18.gif"
-import s from "../../assets/gif/19.gif"
-import t from "../../assets/gif/20.gif"
-import u from "../../assets/gif/21.gif"
-import v from "../../assets/gif/22.gif"
-import w from "../../assets/gif/23.gif"
-import x from "../../assets/gif/24.gif"
-import y from "../../assets/gif/25.gif"
-import z from "../../assets/gif/26.gif"
-import aa from "../../assets/gif/27.gif"
+import bb from "../../assets/gif/coco.gif"
+// import a from "../../assets/gif/1.gif"
+// import b from "../../assets/gif/2.gif"
+// import c from "../../assets/gif/3.gif"
+// import d from "../../assets/gif/4.gif"
+// import e from "../../assets/gif/5.gif"
+// import f from "../../assets/gif/6.gif"
+// import g from "../../assets/gif/7.gif"
+// import h from "../../assets/gif/8.gif"
+// import i from "../../assets/gif/9.gif"
+// import j from "../../assets/gif/10.gif"
+// import k from "../../assets/gif/11.gif"
+// import l from "../../assets/gif/12.gif"
+// import m from "../../assets/gif/13.gif"
+// import n from "../../assets/gif/14.gif"
+// import o from "../../assets/gif/15.gif"
+// import p from "../../assets/gif/16.gif"
+// import q from "../../assets/gif/17.gif"
+// import r from "../../assets/gif/18.gif"
+// import s from "../../assets/gif/19.gif"
+// import t from "../../assets/gif/20.gif"
+// import u from "../../assets/gif/21.gif"
+// import v from "../../assets/gif/22.gif"
+// import w from "../../assets/gif/23.gif"
+// import x from "../../assets/gif/24.gif"
+// import y from "../../assets/gif/25.gif"
+// import z from "../../assets/gif/26.gif"
+// import aa from "../../assets/gif/27.gif"
 
 const HomeCardsFullSize = ({
   containerRef,
@@ -130,44 +130,11 @@ const Home = () => {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-evenly",
-        // alignItems: "center",
+        alignItems: "center",
         // padding: "0 110px",
       }}
     >
       <div className="home-content">
-        <div className="home-colors">
-          <div className="home-colors-track">
-            {[
-              // v,
-              // b,
-              // c,
-              // h,
-              // d,
-              // l,
-              // m,
-              // n,
-              // o,
-              // p,
-              // s,
-              bb,
-            ].map((color, index) => (
-              // h k m s t f
-              <img
-                src={color}
-                key={index}
-                style={{
-                  height: "100%",
-                  width: 150,
-                  borderRadius: "50%",
-                  backgroundColor: "transparent",
-                  flexShrink: 0,
-                  objectFit: "cover",
-                  marginRight: 20,
-                }}
-              />
-            ))}
-          </div>
-        </div>
         <div className="home-text">
           {/* <FaQuoteRight className="home-icon" /> */}
 
@@ -175,8 +142,36 @@ const Home = () => {
           <h1 className="home-title">Engaging</h1>
           <h1 className="home-title">Experiences</h1>
 
-          {/* <p className="home-description">Let’s build something amazing.</p> */}
+          <p className="home-description">Let’s build something amazing</p>
+
+          {/* <img
+            src={bb}
+            style={{
+              height: 4,
+              width: 130,
+              borderRadius: 10,
+              backgroundColor: "transparent",
+              flexShrink: 0,
+              objectFit: "cover",
+              // marginRight: 20,
+            }}
+          /> */}
         </div>
+
+        {/* <div className="home-colors">
+          <img
+            src={bb}
+            style={{
+              height: 100,
+              width: 200,
+              borderRadius: 50,
+              backgroundColor: "transparent",
+              flexShrink: 0,
+              objectFit: "cover",
+              // marginRight: 20,
+            }}
+          />
+        </div> */}
 
         {/* <div
           style={{
